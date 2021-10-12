34301c8f79e6ad33c62e9fbe545afe38bf2afb64
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2021-10-12 16:16:18 +0700

    chore: test

--- src/components/audio/Music.tsx
+++ src/components/audio/Music.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 import { GameSettingsContext } from '@/contexts/gameSettingsContext'
 import { musicTracks } from '@/dto/SoundTrack'
 import ReactAudioPlayer from 'react-audio-player'
