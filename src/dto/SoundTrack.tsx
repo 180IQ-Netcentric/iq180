4bb93b64074009de3547a3b38be990188afb5a2a
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2021-10-12 16:15:24 +0700

    feat: add music and loop

---
+++ src/dto/SoundTrack.tsx
@@ -0,0 +1,14 @@
+import AfterLife from '../assets/audio/afterlife.mp3'
+import Cinema from '../assets/audio/cinema.mp3'
+import Kahoot from '../assets/audio/kahoot.mp3'
+import Wii from '../assets/audio/wii.mp3'
+
+export const musicTrackNames = [
+  'No Music',
+  'Afterlife',
+  'Cinema',
+  'Kahoot',
+  'Wii',
+]
+
+export const musicTracks = [null, AfterLife, Cinema, Kahoot, Wii]
