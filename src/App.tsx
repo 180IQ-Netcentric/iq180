8f34fd3f73c369a652eaf674b64836551fd88588
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2021-10-23 01:51:06 +0700

    feat: connect and disconnect the layer when joining/leaving lobby or game

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import { ThemeContext } from './contexts/themeContext'
 import Lobby from './pages/lobby'
 import { I18nextProvider } from 'react-i18next'
 import i18n from './locales/i18n'
-import SocketContextProvider from './contexts/gameContext'
+import SocketContextProvider from './contexts/socketContext'
 import Socket from './components/Socket'
 
 function App() {
